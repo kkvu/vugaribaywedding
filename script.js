1bf7921d9289cc6663a0fa81b729a7e293c1af8d
Kelly Vu <kvu.1121@gmail.com>
2023-08-07 23:30:07 -0500

    updates

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
-//Contact Form in PHP
-const form = document.querySelector("form"),
-statusTxt = form.querySelector(".button-area span");
-form.onsubmit = (e)=>{
-  e.preventDefault();
-  statusTxt.style.color = "#0D6EFD";
-  statusTxt.style.display = "block";
-  statusTxt.innerText = "Sending your message...";
-  form.classList.add("disabled");
+function subscribe() {
+  var field1 = $("#Name").val();
+  var field2 = $("#Email").val();
 
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "message.php", true);
-  xhr.onload = ()=>{
-    if(xhr.readyState == 4 && xhr.status == 200){
-      let response = xhr.response;
-      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
-        statusTxt.style.color = "red";
-      }else{
-        form.reset();
-        setTimeout(()=>{
-          statusTxt.style.display = "none";
-        }, 3000);
-      }
-      statusTxt.innerText = response;
-      form.classList.remove("disabled");
+  console.log(field1, field2);
+
+  $.ajax({
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSdTj3zmLM6flbsF1U4wi-70x6LnDrCpiEuyiLyOT60WBQyzfg/formResponse?&submit=Submit?usp=pp_url&",
+
+    //add your google form generated numbers below which are also the 'names' of your inputs     
+    data: {
+      "entry.1064657779": field1,
+      "entry.1876428971": field2
+    },
+    type: "POST",
+    dataType: "xml",
+    success: function (d) {
+      // document.getElementById('myemailform').reset()
+      $('#myemailform')[0].reset();
+
+      console.log(field1, field2);
+    },
+    error: function (x, y, z) {
+
+      console.log("boo");
+      // $('#myemailform').trigger('reset');
     }
-  }
-  let formData = new FormData(form);
-  xhr.send(formData);
+  });
+  return false;
+}
+
+
+function contactUs() {
+  var field1 = $("#ContactName").val();
+  var field2 = $("#Message").val();
+
+  console.log(field1, field2);
+
+  $.ajax({
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSfyFq-_qh0zmLBAAnf9qAaHOiW1Z00kQ3442yVhmqoOa0sPoQ/formResponse?&submit=Submit?usp=pp_url&",
+
+    //add your google form generated numbers below which are also the 'names' of your inputs     
+    data: {
+      "entry.805164722": field1,
+      "entry.244595963": field2
+    },
+    type: "POST",
+    dataType: "xml",
+    success: function (d) {
+      // document.getElementById('myemailform').reset()
+      $('#contact')[0].reset();
+
+      console.log(field1, field2);
+    },
+    error: function (x, y, z) {
+
+      console.log("boo");
+      // $('#myemailform').trigger('reset');
+    }
+  });
+  return false;
 }
